3116c876cd4e49a40ae3ba815b6551b72bafe319
Phoenix22121999 <Phoenix>
2020-04-08 22:58:58 +0700

    8/4/2020

---
+++ myTest/php/getLink.php
@@ -0,0 +1,20 @@
+<?php
+$id = $_POST['id'];
+require_once('conn.php');
+$sql = "SELECT movie_id, link
+        FROM movies
+        WHERE movie_id = $id;";
+
+$result = mysqli_query($conn, $sql);
+if ($result) {
+    // Hàm `mysql_fetch_row()` sẽ chỉ fetch dữ liệu một record mỗi lần được gọi
+    // do đó cần sử dụng vòng lặp While để lặp qua toàn bộ dữ liệu trên bảng posts
+    //echo "d";
+    while ($row = mysqli_fetch_row($result)) {
+        $data = array('id' => $row[0], 'link' => $row[1]);
+        //echo $target_dir . "-" .$row[1]. "-" 
+    }
+    $data_json = json_encode($data);
+    echo $data_json;
+    //mysqli_free_result($result);
+}
--- myTest/php/loadPage.php
+++ myTest/php/loadPage.php
@@ -1,61 +1,62 @@
 <?php
-    /*$dataReceive = file_get_contents('php://input');
+/*$dataReceive = file_get_contents('php://input');
     echo var_dump($dataReceive);
     $data = json_decode($dataReceive,true);
     echo var_dump($data);
     echo $dataReceive;
     echo $data['genre'];*/
-    require('conn.php');
-    //echo "kết nối thành công";
-    $sql = "SELECT movies.movie_id, movies.title 
+require('conn.php');
+//echo "kết nối thành công";
+$sql = "SELECT movies.movie_id, movies.title 
             FROM movies,movie_genre 
             WHERE movies.movie_id = movie_genre.movie_id AND movie_genre.genre_id = 1
             ORDER BY movie_id DESC 
-            LIMIT 5;"; 
-    $result = mysqli_query($conn, $sql);    
-    $data=array();
-    $data_tmp=array();
-    //echo "lấy đc kết quả";
-    if ($result) {
-        // Hàm `mysql_fetch_row()` sẽ chỉ fetch dữ liệu một record mỗi lần được gọi
-        // do đó cần sử dụng vòng lặp While để lặp qua toàn bộ dữ liệu trên bảng posts
-        //echo "d";
-        $i=1;
-        while ($row = mysqli_fetch_row($result)) {
-            $target_dir = "picture/poster/HanhDong/" . $row[0] .".jpg";
-            $data_tmp{'movie_'.$i} = array( 'link'=>$target_dir,'title' =>$row[1]);
-            //echo $target_dir . "-" .$row[1]. "-" ;
-            $i++;
-        }
-        $data["HanhDong"]=$data_tmp;
-        $data_json = json_encode($data);
-        //echo $data_json;
-        //mysqli_free_result($result);
-    }
-    $sql = "SELECT movies.movie_id, movies.title 
-    FROM movies,movie_genre 
-    WHERE movies.movie_id = movie_genre.movie_id AND movie_genre.genre_id = 2
-    ORDER BY movie_id DESC 
-    LIMIT 5;"; 
-    $result = mysqli_query($conn, $sql);    
-    $data_tmp=array();
-    //echo "lấy đc kết quả";
-    if ($result) {
+            LIMIT 4;";
+$result = mysqli_query($conn, $sql);
+$data = array();
+$data_tmp = array();
+//echo "lấy đc kết quả";
+if ($result) {
     // Hàm `mysql_fetch_row()` sẽ chỉ fetch dữ liệu một record mỗi lần được gọi
     // do đó cần sử dụng vòng lặp While để lặp qua toàn bộ dữ liệu trên bảng posts
     //echo "d";
-    $i=1;
+    $i = 1;
     while ($row = mysqli_fetch_row($result)) {
-        $target_dir = "picture/poster/KinhDi/" . $row[0] .".jpg";
-        $data_tmp{'movie_'.$i} = array( 'link'=>$target_dir,'title' =>$row[1]);
+        $target_dir = "picture/poster/HanhDong/" . $row[0] . ".jpg";
+        $data_tmp{
+            'movie_' . $i} = array('id' => $row[0], 'link' => $target_dir, 'title' => $row[1]);
         //echo $target_dir . "-" .$row[1]. "-" ;
         $i++;
     }
-    $data["KinhDi"]=$data_tmp;
-   
+    $data["HanhDong"] = $data_tmp;
+    $data_json = json_encode($data);
+    //echo $data_json;
+    //mysqli_free_result($result);
+}
+$sql = "SELECT movies.movie_id, movies.title 
+    FROM movies,movie_genre 
+    WHERE movies.movie_id = movie_genre.movie_id AND movie_genre.genre_id = 2
+    ORDER BY movie_id DESC 
+    LIMIT 4;";
+$result = mysqli_query($conn, $sql);
+$data_tmp = array();
+//echo "lấy đc kết quả";
+if ($result) {
+    // Hàm `mysql_fetch_row()` sẽ chỉ fetch dữ liệu một record mỗi lần được gọi
+    // do đó cần sử dụng vòng lặp While để lặp qua toàn bộ dữ liệu trên bảng posts
+    //echo "d";
+    $i = 1;
+    while ($row = mysqli_fetch_row($result)) {
+        $target_dir = "picture/poster/KinhDi/" . $row[0] . ".jpg";
+        $data_tmp{
+            'movie_' . $i} = array('id' => $row[0], 'link' => $target_dir, 'title' => $row[1]);
+        //echo $target_dir . "-" .$row[1]. "-" ;
+        $i++;
+    }
+    $data["KinhDi"] = $data_tmp;
+
     $data_json = json_encode($data);
     echo $data_json;
     mysqli_free_result($result);
-    }
-    $conn->close();
-?>
+}
+$conn->close();
